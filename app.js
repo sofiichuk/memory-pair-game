f530bda3eae4d935cd5d31c045870d3d9e0737eb
m.sofiichuk <koyap@ukr.net>
2022-09-21 01:02:00 +0300

    2nd commit

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
+//=============opening cards=============
 const cards = document.querySelectorAll('.card');
-
-function showPic() {
+function showHiddenPic() {
     this.classList.toggle('selectedCard')
-};
+}
+cards.forEach(card => card.addEventListener('click', showHiddenPic));
 
-cards.forEach(card => card.addEventListener('click', showPic));
+//============comparing cards==============
+
+// (A) FLAG FOR "ALREADY CLICKED".
+let booleanFlag = false;
+let firstCard, secondCard;
+
+if (!booleanFlag) {
+    booleanFlag = true;
+    firstCard = this;
+}
+// (B) FUNCTION - WILL ONLY RUN IF NOT CLICKED
+function getTwoCardsToCompare () { if (!booleanFlag) {
+    // (B1) SET CLICKED TO TRUE
+    booleanFlag = true;
+
+    // (B2) DO YOUR PROCESSING HERE
+    alert("Something is done!");
+
+    // (B3) RE-ENABLE AFTER PROCESSING IF YOU WANT
+    // booleanFlag = false;
+}}
+
+//==================================================================
+
+// firstCard=document.querySelectorAll('.selectedCard');
+// const clickedCards = document.querySelectorAll('.card.selectedCard');
+// if (clickedCards[0].getAttribute.src === clickedCards[1].getAttribute.src)
+//    {console.log('Same!')}
+// console.log(clickedCards[0], clickedCards[1])
